52f1236b65871f82c0b1466f0024dbebaaea7b93
Seth Sun <sithsun@gmail.com>
2023-06-24 21:51:19 -0400

    Change message

--- inquirer.js
+++ inquirer.js
@@ -16,7 +16,7 @@ connection.connect((err) => {
       console.error('Error connecting to the database: ' + err.stack);
       return;
     }
-    console.log('Connected to the database as ID ' + connection.threadId);
+    console.log('Connected to the MySQL employeetracker_db successfully');
 
     // ---- Inquirer Queries ----
 
