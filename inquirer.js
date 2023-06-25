7fd3c7cae8ca2d537273209315767b03949b6628
Seth Sun <sithsun@gmail.com>
2023-06-24 21:38:09 -0400

    changed port for mysql2 to run properly.

--- inquirer.js
+++ inquirer.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 // Create a MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3001,
+  port: 3306,
   user: 'root',
   password: 'root',
   database: 'employeetracker_db',
