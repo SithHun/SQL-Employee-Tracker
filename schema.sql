ade0cedcc88fb4829238fe7fa03c1cd1972d251c
Seth Sun <sithsun@gmail.com>
2023-06-24 20:05:50 -0400

    Added role table that reference department id

--- schema.sql
+++ schema.sql
@@ -1,8 +1,17 @@
 DROP DATABASE IF EXISTS employeetracker_db;
 CREATE DATABASE employeetracker_db;
 
--- DEPARTMENT Table
+-- DEPARTMENT TABLE
 CREATE TABLE department (
   id INT PRIMARY KEY,
   name VARCHAR(30)
+);
+
+-- ROLE TABLE
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department (id)
 );
